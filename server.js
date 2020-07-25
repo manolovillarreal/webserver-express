f351f74096c2d57f1491ee3c6b6e2939f121f2a0
Manuel Villarreal <manolovillarreal@hotmail.com>
2020-07-25 15:19:25 -0500

    Primer Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+require("./hbs/helpers.js");
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("home", { nombre: "manolo villarreal" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.listen(port, () => {
+  console.log(`Escuchando puerto ${port}`);
+});
